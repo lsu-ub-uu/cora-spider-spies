c81186569474d96b0e5afc5f3bb97b38e0cab822
olovm <olov.mckie@ub.uu.se>
2022-10-07 13:06:30 +0000

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * The spider spies module provides common spies used when testing Cora projects.
+ */
+module se.uu.ub.cora.spider.spies {
+
+	requires org.testng;
+	requires se.uu.ub.cora.data;
+	requires se.uu.ub.cora.spider;
+	requires se.uu.ub.cora.httphandler;
+	requires transitive se.uu.ub.cora.testutils;
+
+	exports se.uu.ub.cora.spider.spies;
+}
